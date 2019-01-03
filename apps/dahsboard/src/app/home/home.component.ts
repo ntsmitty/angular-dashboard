85025f53532051336db147a330ecad8e7686280d
ntsmitty <nicktsmith7@gmail.com>
2019-01-03 09:50:28 -0800

    init commit

---
+++ apps/dahsboard/src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  //changed from workshop-home -> app-home 
+  selector: 'workshop-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
